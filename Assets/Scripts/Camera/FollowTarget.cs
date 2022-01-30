57d67ba382c325829ce46b08f10449187c803143
Thomas Haygarth <haygarth.thomas@gmail.com>
2022-01-30 16:59:15 +0000

    Implemented basic player controller using unit physics

---
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField]
+    private bool m_followXaxis = true;
+    [SerializeField]
+    private bool m_followYaxis = false;
+    [SerializeField]
+    private bool m_followZaxis = false;
+
+    [SerializeField]
+    private Transform m_targetTransform = null;
+
+    private Transform m_cachedTransform = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cachedTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosisiton = m_cachedTransform.position;
+        Vector3 targetPosition = m_targetTransform.position;
+
+        if (m_followXaxis)
+        {
+            newPosisiton.x = targetPosition.x;
+        }
+        if (m_followYaxis)
+        {
+            newPosisiton.y = targetPosition.y;
+        }
+        if (m_followZaxis)
+        {
+            newPosisiton.z = targetPosition.z;
+        }
+
+        m_cachedTransform.position = newPosisiton;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool m_isGrounded = false;
+
+    private Rigidbody2D m_rigidBody;
+    private BoxCollider2D m_collider;
+
+    [SerializeField]
+    private float m_runAcceleration = 5.0f;
+    [SerializeField]
+    private float m_maxHorizontalVelocity = 15.0f;
+
+    [SerializeField]
+    private float m_jumpForce = 10.0f;
+
+    [SerializeField]
+    private float m_jumpCooldown = 0.1f;
+
+    private float m_currentJumpCooldown = 0.0f;
+
+    private bool m_isJumping = false;
+    private bool m_playJumpSfx = false;
+    private bool m_playLandSfx = false;
+
+
+    private int ground_mask = 0;
+
+    private void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        m_collider = GetComponent<BoxCollider2D>();
+        ground_mask = 1 << LayerMask.NameToLayer("Ground");
+    }
+
+    private void Update()
+    {
+        if (m_playJumpSfx)
+        {
+            // play
+            m_playJumpSfx = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        bool update_velocity = false;
+        var current_velocity = m_rigidBody.velocity;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            update_velocity = true;
+            current_velocity.x -= m_runAcceleration * Time.fixedDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            update_velocity = true;
+            current_velocity.x += m_runAcceleration * Time.fixedDeltaTime;
+        }
+
+
+        if (m_currentJumpCooldown <= 0.0f)
+        {
+            if (CheckGrounded())
+            {
+                if (Input.GetKey(KeyCode.W) ||
+                    Input.GetKey(KeyCode.UpArrow) ||
+                    Input.GetKey(KeyCode.Space))
+                {
+                    m_currentJumpCooldown = m_jumpCooldown;
+                    update_velocity = true;
+                    current_velocity.y = m_jumpForce * m_rigidBody.gravityScale;
+                    m_isJumping = true;
+                    m_playJumpSfx = true;
+                }
+            }
+        }
+        else
+        {
+            m_currentJumpCooldown -= Time.fixedTime;
+        }
+
+        if (update_velocity)
+        {
+            m_rigidBody.velocity = current_velocity;
+        }
+    }
+
+    private bool CheckGrounded()
+    {
+        //var result = Physics2D.BoxCast(m_rigidBody.position + m_collider.offset,
+        //                               m_collider.size,
+        //                               0.0f,
+        //                               Vector2.down,
+        //                               0.1f);
+
+        //return result.transform != null;
+        return Physics2D.IsTouchingLayers(m_collider, ground_mask);
+    }
+}
